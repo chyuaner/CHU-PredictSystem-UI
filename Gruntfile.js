23861a0092cfbfbf768ef81358f1610b8352f231
Yuan Chiu <chyuaner@gmail.com>
2015-07-06 23:09:26 +0800

    first commit, create base html

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'dest/css/index.css': 'src/scss/index.scss',
+        }
+      }
+    },
+
+    copy: {
+      minjs: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/',
+            src: ['jquery/dist/jquery.min.js', 'foundation/js/foundation.min.js'],
+            dest: 'dest/assets/js/'
+          }
+        ]
+      }
+    },
+
+    connect: {
+        server: {
+            options: {
+                hostname: '*',
+                base: 'dest'
+            }
+        }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      frontend: {
+        files: [ 'dest/**/*.js', 'dest/**/*.html' ],
+      },
+      stylesheet: {
+        files: [ 'src/scss/**/*.scss' ],
+        tasks: [ 'sass' ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['copy', 'sass', 'connect', 'watch']);
+}

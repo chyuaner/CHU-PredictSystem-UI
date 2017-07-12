51813db6225cb534c92b735f652434a2991b70da
Yang <Yang@Yang-MacBook-Pro.local>
2017-07-12 22:22:03 +0800

    login send-btn判斷式

--- dest/js/login.js
+++ dest/js/login.js
@@ -767,7 +767,7 @@ $('#return-btn').click(function(e){
 $('#send-btn').click(function (e){
   var inputRegisterData = getRegisterData();
 
-  if((inputRegisterData.email.val() == null) || (inputRegisterData.location.val() == null) || (inputRegisterData.schoolName.val() == null) || (inputRegisterData.identity.val() == null)){}
+  if((inputRegisterData.email == null) || (inputRegisterData.location == null) || (inputRegisterData.schoolName == null) || (inputRegisterData.identity == null)){}
   // 沒有問題，開始向後端要資料
   else {
     e.preventDefault();

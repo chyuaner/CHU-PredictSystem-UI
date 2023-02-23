fde7fb4e7c0837bea024b5fdd4f8c2211b358447
林廣學 <tomkhlin@g.chu.edu.tw>
2023-02-23 14:32:01 +0800

    完成112學測落點分析網頁介面調整與修正&LIFF調整

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,10 +65,10 @@ module.exports = function(grunt) {
           includes: {
             footer: './src/footer.inc',
             oldie: './src/old-ie.inc',
-            // analytics: './src/ga.inc',
-            // meta: './src/meta.inc',
-            // body_tagmanager: './src/body_tagmanager.inc',
-            // head_tagmanager: './src/head_tagmanager.inc'
+            analytics: './src/ga.inc',
+            meta: './src/meta.inc',
+            body_tagmanager: './src/body_tagmanager.inc',
+            head_tagmanager: './src/head_tagmanager.inc'
           }
         }
       }
--- src/gsat/js/send-predict-data-to-liff.js
+++ src/gsat/js/send-predict-data-to-liff.js
@@ -131,13 +131,6 @@ function sentToLIFF(data) {
     }
 }
 
-liff.init({
-    // liffId: '1657321772-40ezKwvL'
-    liffId: '1657337645-Eop8Wj1r'
-}).catch((err)=>{
-    alert(err);
-});
-
 let form_input = document.getElementById('input-form');
 form_input.onsubmit = function (e)  {
     e.preventDefault();

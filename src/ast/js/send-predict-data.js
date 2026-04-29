18873f09ba0d869dfe5189ccb56b2c41c2a220de
Yuan Chiu <chyuaner@gmail.com>
2026-04-30 05:42:38 +0800

    fix: basePredictSystemUrl 接到正式網址

--- src/ast/js/send-predict-data.js
+++ src/ast/js/send-predict-data.js
@@ -1,6 +1,6 @@
-var basePredictSystemUrl =
+//var basePredictSystemUrl =
     "https://kqxl54nx-7176.asse.devtunnels.ms/api/ast/analysis";
-// var basePredictSystemUrl = "api/ast/analysis";
+ var basePredictSystemUrl = "api/ast/analysis";
 var max_input_score = 15;
 var querying = false;
 
